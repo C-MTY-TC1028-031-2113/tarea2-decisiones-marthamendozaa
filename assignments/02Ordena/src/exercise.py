5be536c047f731f701626acce3a43d3d68fb45d9
Martha <a01284654@itesm.mx>
2021-08-29 17:24:23 -0500

    290821

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,8 +3,30 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    
-print("Hola")
+    if x < y and x < z and y < z:
+        print(x)
+        print(y)
+        print(z)
+    if x < y and x < z and y > z:
+        print(x)
+        print(z)
+        print(y)
+    if y < x and y < z and x < z:
+        print(y)
+        print(x)
+        print(z)
+    if y < x and y < z and z < x:
+        print(y)
+        print(z)
+        print(x)
+    if z < x and z < y and x < y:
+        print(z)
+        print(x)
+        print(y)
+    if z < x and z < y and y < x:
+        print(z)
+        print(y)
+        print(x)
 
 if __name__=='__main__':
     main()
