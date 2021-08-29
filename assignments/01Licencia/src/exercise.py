8219b5694c1239c1eab561dee1514f84e258f639
Martha <a01284654@itesm.mx>
2021-08-29 17:07:14 -0500

    2910123

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,6 +1,18 @@
 
 def main():
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    if edad >= 18:
+        id = input("¿Tienes identificación oficial? (s/n): ")
+        if id == "s":
+            print("Trámite de licencia concedido")
+        elif id == "n":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+
+    else:
+        print("No cumples requisitos")
+
 
 
 if __name__ == '__main__':
