8a9bcfb26c48c0314e1b19bf736ef691536d16ef
Martha <a01284654@itesm.mx>
2021-08-29 20:02:33 -0500

    290821

--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -5,21 +5,21 @@ def main():
     a=int(input("Da el valor de a: "))
     b=int(input("Da el valor de b: "))
     c=int(input("Da el valor de c: "))
-    x1 = (- b + math.sqrt(b**2 - 4*a*c)) / 2*a
-    x2 = (- b - math.sqrt(b**2 - 4*a*c)) / 2*a
     disc = b**2 - 4*a*c
-    d = -b/(2*a)
-    if a == 0 and b == 0 :
+    if a == 0 and b == 0:
         print("No tiene solución")
     elif a == 0 and b!=0:
-         x = -c/b
-         print(x)
+        x = -c/b
+        print(x)
     elif a != 0 and b != 0 and disc > 0:
+        x1 = (- b + math.sqrt(b**2 - 4*a*c)) / 2*a
+        x2 = (- b - math.sqrt(b**2 - 4*a*c)) / 2*a
         print(x1)
         print(x2)
     elif a != 0 and b != 0 and disc < 0:
         print("Raices complejas")
     elif a != 0 and b != 0 and disc == 0:
+        d = -b/(2*a)
         print(d)
 
 
